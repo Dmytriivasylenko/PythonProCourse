e6f2f94c28e118d5832125a2e5824e598c1841d1
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2024-07-01 23:55:30 +0200

    fitness

--- app.py
+++ app.py
@@ -1,87 +1,9 @@
+import os
 from flask import Flask, render_template, request, redirect, session, json
-import sqlite3
+import utils
 
 app = Flask(__name__)
 app.secret_key = b'_343435#y2L"F4Q8z\n\xec]/'
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-class SQLiteDatabase:
-    def __init__(self, db_path):
-        self.db_path = db_path
-        self.connection = None
-
-    def __enter__(self):
-        self.connection = sqlite3.connect(self.db_path)
-        self.connection.row_factory = dict_factory
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.connection:
-            self.connection.close()
-
-    def fetch_all(self, query, params=()):
-        cursor = self.connection.cursor()
-        cursor.execute(query, params)
-        res = cursor.fetchall()
-        return res if res else []
-
-    def fetch_one(self, query, params=()):
-        cursor = self.connection.cursor()
-        cursor.execute(query, params)
-        res = cursor.fetchone()
-        return res if res else None
-
-    def execute_query(self, query, params=(), fetch_one=False, fetch_all=True):
-        cur = self.connection.cursor()
-        cur.execute(query, params)
-        if fetch_one:
-            result = cur.fetchone()
-        elif fetch_all:
-            result = cur.fetchall()
-        else:
-            result = None
-        self.connection.commit()
-        return result
-
-def check_credentials(login, password):
-    query = 'SELECT * FROM user WHERE login = ? AND password = ?'
-    with SQLiteDatabase('db.db') as db:
-        user = db.execute_query(query, (login, password), fetch_one=True)
-        return user is not None
-
-def select_method(self, table, condition=None, columns=None, fetch_all=True, join=None):
-    query = 'SELECT'
-    if columns:
-        query += ', '.join(columns)
-    else:
-        query += '* '
-    query += f'FROM {table} '
-
-    if join:
-        for join_table, join_condition in join.items():
-            query += f'JOIN {join_table} ON {join_condition} '
-
-    if condition:
-        conditions = []
-        for key, value in condition.items():
-            conditions.append(f'{key} = ?')
-        query += 'WHERE ' + ' AND '.join(conditions)
-
-    cursor = self.connection.cursor()
-    cursor.execute(query, tuple(condition.values()) if condition else ())
-
-    if fetch_all:
-        result = cursor.fetchall()
-    else:
-        result = cursor.fetchone()
-
-    return result if result else None
-
 
 
 def login_required(func):
@@ -93,8 +15,6 @@ def login_required(func):
 
     return wrapper
 
-
-
 @app.route("/", methods=["GET", "POST", "PUT"])
 def index():
     return "current method:" + str(request.method)
@@ -104,7 +24,7 @@ def get_register():
     if request.method == 'POST':
         form_data = request.form
         query = 'INSERT INTO user (login, password, birth_date, phone) VALUES (?, ?, ?, ?)'
-        with SQLiteDatabase("db.db") as db:
+        with utils.SQLiteDatabase("db.db") as db:
             db.execute_query(query, (form_data["login"], form_data["password"],
                                      form_data["birth_date"], form_data["phone"]))
         return redirect('/login')
@@ -113,7 +33,7 @@ def get_register():
 @app.route('/user', methods=['GET'])
 @login_required
 def get_users():
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         res = db.fetch_all('SELECT * FROM user')
     return render_template('user.html', users=res)
 
@@ -122,11 +42,11 @@ def user_details(user_id):
     if request.method == 'POST':
         form_data = request.form
         query = 'UPDATE user SET login = ?, phone = ?, birth_date = ? WHERE id = ?'
-        with SQLiteDatabase("db.db") as db:
+        with utils.SQLiteDatabase("db.db") as db:
             res = db.execute_query(query, (["login", form_data], form_data["phone"], form_data["birth_date"], user_id))
         return redirect('get_users', user_id=res)
 
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         res = db.fetch_one('SELECT login, phone, birth_date FROM user WHERE id = ?', (user_id,))
     return render_template('user_detail.html', user=res)
 @login_required
@@ -135,8 +55,8 @@ def login():
     if request.method == 'POST':
         login = request.form['login']
         password = request.form['password']
-        if check_credentials(login, password):
-            with SQLiteDatabase("db.db") as db:
+        if utils.check_credentials(login, password):
+            with utils.SQLiteDatabase("db.db") as db:
                 user = db.execute_query('SELECT * FROM user WHERE login = ?', (login,), fetch_one=True)
             session['user_id'] = user['id'] if user else None
             return redirect('/user')
@@ -154,13 +74,13 @@ def get_user(user_id):
     if request.method == 'POST':
         form_data = request.form
         query = 'UPDATE user SET login = ?, phone = ?, birth_date = ? WHERE id = ?'
-        with SQLiteDatabase("db.db") as db:
+        with utils.SQLiteDatabase("db.db") as db:
             db.execute_query(query, (form_data["login"], form_data["phone"], form_data["birth_date"], user_id))
         return 'User data modified'
     elif request.method == 'GET':
-        with SQLiteDatabase("db.db") as db:
+        with utils.SQLiteDatabase("db.db") as db:
             res = db.fetch_one('SELECT login, phone, birth_date FROM user WHERE id = ?', (user_id,))
-        return json(res)
+        return res
 
 
 @login_required
@@ -169,9 +89,9 @@ def user_reservations():
     if request.method == 'GET':
         user_id = session.get('user_id', None)
         if not user_id:
-            return redirect('login')  # перенаправлення на сторінку входу, якщо користувач не увійшов
+            return redirect('login')
 
-        with SQLiteDatabase('db.db') as db:
+        with utils.SQLiteDatabase('db.db') as db:
             reservations = db.execute_query(
                 'reservations',
                 condition={'user_id': user_id},
@@ -179,7 +99,6 @@ def user_reservations():
                 fetch_all=True
             )
         return render_template('reservations.html', user_reservation=reservations)
-
     elif request.method == 'POST':
         return "create user reservation"
 
@@ -194,12 +113,12 @@ def user_reservation(reservation_id):
             return 'New date and time are required'
 
         query = 'UPDATE reservations SET date=?, time=? WHERE id=?'
-        with SQLiteDatabase('db.db') as db:
+        with utils.SQLiteDatabase('db.db') as db:
             db.execute_query(query, (new_date, new_time, reservation_id))
 
         return redirect('user_reservations')
 
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         res = db.fetch_one('SELECT * FROM reservations WHERE id = ?', (reservation_id,))
     return render_template('reservation_detail.html', reservation=res)
 
@@ -207,7 +126,7 @@ def user_reservation(reservation_id):
 @app.route('/user/reservations/<reservation_id>/delete', methods=['POST'])
 def delete_reservation(reservation_id):
     query = 'DELETE FROM reservations WHERE id=?'
-    with SQLiteDatabase('db.db') as db:
+    with utils.SQLiteDatabase('db.db') as db:
         db.execute_query(query, (reservation_id,))
     return redirect('user_reservations')
 
@@ -218,14 +137,14 @@ def trainer_rating(gym_id, trainer_id):
     if request.method == 'POST':
         rating = request.form.get('rating')
         review_text = request.form.get('review')
-        with SQLiteDatabase("db.db") as db:
+        with utils.SQLiteDatabase("db.db") as db:
             db.execute_query(
                 "INSERT INTO review (trainer_id, gym_id, user_id, rating, review) VALUES (?, ?, ?, ?, ?)",
                 (trainer_id, gym_id, user_id, rating, review_text)
             )
         return redirect('trainer_rating', gym_id=gym_id, trainer_id=trainer_id)
 #отримати відгук який дав користувач і заповни ім форму додатковий запит на відгук користувача
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         reviews = db.select_method(
             'review',
             join={'trainer': 'review.trainer_id = trainer.id', 'gym': 'review.gym_id = gym.id', 'user': 'review.user_id = user.id'},
@@ -237,40 +156,39 @@ def trainer_rating(gym_id, trainer_id):
 @login_required
 @app.route('/fitness_center', methods=['GET'])
 def fitness_center():
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         res = db.fetch_all('SELECT id, name, adress, contacts FROM fitness_center')
     return render_template('fitness_center.html', fitness_center=res)
 
 @app.route('/fitness_center/<gym_id>', methods=['GET'])
 def get_fitness_center(gym_id):
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         center = db.fetch_one('SELECT id, name, address FROM fitness_center WHERE id = ?', (gym_id,))
     return render_template('fitness_center_id.html', fitness_center=center)
 @login_required
 @app.route('/fitness_center/<gym_id>/trainer', methods=['GET'])
 def get_trainers(gym_id):
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         trainers = db.fetch_all('SELECT id, name FROM trainer WHERE gym_id = ?', (gym_id,))
     return render_template('trainers.html', trainers=trainers, gym_id=gym_id)
 
 @app.route('/fitness_center/<int:gym_id>/services', methods=['GET'])
 def get_services(gym_id):
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         services = db.fetch_all('SELECT * FROM services WHERE gym_id = ?', (gym_id,))
     return render_template('services.html', services=services)
 
 @app.route('/fitness_center/<int:gym_id>/services/<int:service_id>', methods=['GET'])
 def get_service_info(gym_id, service_id):
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         service = db.fetch_one('SELECT * FROM services WHERE gym_id = ? AND id = ?', (gym_id, service_id))
     return render_template('service_detail.html', service=service)
 
 @app.route('/fitness_center/<gym_id>/loyalty_programs', methods=['GET'])
 def get_loyalty_programs(gym_id):
-    with SQLiteDatabase("db.db") as db:
+    with utils.SQLiteDatabase("db.db") as db:
         programs = db.fetch_all('SELECT * FROM loyalty_programs WHERE gym_id = ?', (gym_id,))
     return render_template('loyalty_programs.html', loyalty_programs=programs)
-
 
 
 @app.route('/checkout', methods=['GET'])
@@ -285,7 +203,32 @@ def user_checkout_added():
 def user_checkout_update():
     return 'Balance was updated'
 
+@login_required
+@app.route('/shedule', methods=['GET'])
+def shedule():
+    with utils.SQLiteDatabase("db.db") as db:
+        res = db.fetch_all('SELECT id, date, trainer, start_time, end_time FROM shedule')
+    return render_template('shedule.html', shedule=res)
+
+@login_required
+@app.route('/shedule/add', methods=['GET', 'POST'])
+def add_shedule():
+    if request.method == 'POST':
+        date = request.form.get('date')
+        trainer = request.form.get('trainer')
+        start_time = request.form.get('start_time')
+
+        with utils.SQLiteDatabase("db.db") as db:
+            db.execute_query(
+                'INSERT INTO shedule (date, trainer, start_time, end_time) VALUES (?, ?, ?)',
+                (date, trainer, start_time)
+            )
+
+        return redirect('shedule')
+
+    return render_template('add_shedule.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+app.secret_key = os.environ.get('SESSION_SECRET_KEY')
